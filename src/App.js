aa5c9ca07808d01e24965819f1589a429c1cf2be
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-03-22 22:06:31 +0400

    Loading Questions from a Fake API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import { act } from "react-dom/test-utils";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialSate = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unkown");
+  }
+}
 
 export default function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialSate);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
