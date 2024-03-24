0e5bf71234fd7f24e94b7a4291b66a6764282f2f
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-03-24 17:12:13 +0400

    Moving to the Next Question

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialSate = {
   questions: [],
@@ -40,6 +41,13 @@ function reducer(state, action) {
             : state.points,
       };
     }
+    case "nextQuestion": {
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    }
     default:
       throw new Error("Action unkown");
   }
@@ -71,11 +79,14 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
